9f7a243c73f66d35242d8612bdbe1c241c1e725c
Omer Siddiqui <omer@Maryums-MacBook.local>
2018-07-28 13:41:01 -0400

    animation testing

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/////////////////////////////////
+//        initialize WOW.js  ///
+///////////////////////////////
+
+new WOW().init();
+
+/////////////////////////////////
+//        typewriter         ///
+///////////////////////////////
+
+// new TypeIt('.type', {
+//     speed: 70
+// })
+// .type('"Become a student of <span class="highlight">innovation</span>')
+// .pause(500)
+// .delete(10)
+// .pause(500)
+// .type('<span class="highlight">revision</span>')
+// .pause(500)
+// .delete(8)
+// .type('<span class="highlight">change</span>.')
+// .pause(500)
+// .type(' It is the only thing that will remain <span class="highlight">constant.</span>"')
+
+
+// “Become a student of <span class="highlight">change</span>. It is the only thing that will remain <span class="highlight">constant</span>.” -Anthony D'angelo
